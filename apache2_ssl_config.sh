ad15a2d1b373164ee1fec557261697a682bce780
alanmeyer <alanmeyer@yahoo.com>
2014-08-22 14:45:01 -0700

    SSL Update

--- apache2_ssl_config.sh
+++ apache2_ssl_config.sh
@@ -4,17 +4,12 @@
 # https://github.com/alanmeyer/postinstall
 
 GIT_SHARE=<from_config_script>
+HOSTNAME=<from_config_script>
+DOMAIN=<from_config_script>
+MAIL=<from_config_script>
+WEB=<from_config_script>
 APACHE_SSL_DIR=/etc/apache2/ssl
 APACHE_SITES_DIR=/etc/apache2/sites-available
-
-if [ "$#" -ne 3 ]; then
-    echo "usage: apache2_ssl_config.sh hostname domain maildomain webdomain"
-    exit 1;
-fi
-
-echo "fqdn:        " $1.$2
-echo "mail domain: " $3
-echo "web domain:  " $4
 
 a2enmod ssl
 service apache2 stop
@@ -33,9 +28,9 @@ mv apache2_ssl_ssl.crt          $APACHE_SSL_DIR/ssl.crt
 mv apache2_ssl_ca-bundle.pem    $APACHE_SSL_DIR/ca-bundle.pem
 mv apache2_ssl_000-default.conf $APACHE_SITES_DIR/000-default.conf
 
-sed -i 's,\(ServerName\),\1 '"$4"',g'               $APACHE_SITES_DIR/000-default.conf
-sed -i 's,\(Redirect\),\1 https://'"$4"',g'         $APACHE_SITES_DIR/000-default.conf
-sed -i 's,\(ServerAdmin\),\1 webmaster@'"$3"',g'    $APACHE_SITES_DIR/000-default.conf
+sed -i 's,\(ServerName\).*,\1 '"$WEB"',g'             $APACHE_SITES_DIR/000-default.conf
+sed -i 's,\(Redirect\).*,\1 / https://'"$WEB"',g'     $APACHE_SITES_DIR/000-default.conf
+sed -i 's,\(ServerAdmin\).*,\1 webmaster@'"$MAIL"',g' $APACHE_SITES_DIR/000-default.conf
 
 wget $GIT_SHARE/apache2_ssl_private-encrypted.key
 openssl rsa -in apache2_ssl_private-encrypted.key -out apache2_ssl_private.key
